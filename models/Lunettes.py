40504d7e052a9f6b26a764840d9a21efd972f2bd
Cole McGrath <my_acclive@outlook.com>
2021-01-29 04:32:38 +0100

    Edited Lunettes Model Class
    Removed the MatLens and Colors Assossiation Columns + Fixed __init__

--- models/Lunettes.py
+++ models/Lunettes.py
@@ -9,27 +9,28 @@ class Lunettes(BaseModel):
     form_id = Column(Integer, ForeignKey('form.id'))
     gender_id = Column(Integer, ForeignKey('gender.id'))
     marque_id = Column(Integer, ForeignKey('marque.id'))
-    material_Fond_id = Column(Integer, ForeignKey('material_Fond.id'))
-    material_Front_id = Column(Integer, ForeignKey('material_Front.id'))
-    material_lens_id = Column(Integer, ForeignKey('material_lens.id'))
+    material_fond_id = Column(Integer, ForeignKey('material_Fond.id'))
+    material_front_id = Column(Integer, ForeignKey('material_Front.id'))
     structure_id = Column(Integer, ForeignKey('structure.id'))
     taille_lunettes_id = Column(Integer, ForeignKey('taille_lunettes.id'))
     taille_temple_id = Column(Integer, ForeignKey('taille_temple.id'))
+    year = Column(String)
 
-    def _init_(self, id, name:str, 
-                categorie:int=None, form:int=None, gender:int=None, marque:int=None, 
-                material_fond:int=None, material_front:int=None, material_lens:int=None, 
-                structure:int=None, 
-                taille_lunettes:int=None, taille_temple:int=None):
+    def _init_(self, id, name, *,
+                categorie_id:int=None, form_id:int=None, gender_id:int=None, marque_id:int=None, 
+                material_fond_id:int=None, material_front_id:int=None, 
+                structure_id:int=None, 
+                taille_lunettes_id:int=None, taille_temple_id:int=None,
+                year:str=None):
         self.id = id
         self.name = name
-        self.categorie_id = categorie
-        self.form_id = form
-        self.gender_id = gender
-        self.marque_id = marque
-        self.material_Fond_id = material_fond
-        self.material_Front_id = material_front
-        self.material_lens_id = material_lens
-        self.structure_id = structure
-        self.taille_lunettes_id = taille_lunettes
-        self.taille_temple_id = taille_temple
+        self.categorie_id = categorie_id
+        self.form_id = form_id
+        self.gender_id = gender_id
+        self.marque_id = marque_id
+        self.material_fond_id = material_fond_id
+        self.material_front_id = material_front_id
+        self.structure_id = structure_id
+        self.taille_lunettes_id = taille_lunettes_id
+        self.taille_temple_id = taille_temple_id
+        self.year = year
